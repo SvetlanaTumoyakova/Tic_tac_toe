dd41110df379aec93befa6a5a6f562d54a88080a
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2026-02-14 19:26:08 +0700

    added a view for the playing field and the score of the players

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tic_tac_toe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
                 if(getPlayerOneName.isEmpty() || getPlayerTwoName.isEmpty()){
                     Toast.makeText(MainActivity.this, "Please enter player name",
                         Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent intent = new Intent(MainActivity.this, PlayingField.class);
+                    intent.putExtra("playerOne", getPlayerOneName);
+                    intent.putExtra("playerTwo", getPlayerTwoName);
+                    startActivity(intent);
                 }
             }
         });
