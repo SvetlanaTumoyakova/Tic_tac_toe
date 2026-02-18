448e4aa5edfcf7bd8fe1866bf3725df8b7de0610
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2026-02-19 06:46:16 +0700

    feat(game): implement core Tic‑Tac‑Toe gameplay for two players
    - added win condition checking (rows, columns, diagonals)
    - implemented winner dialog with "Play Again" button
    - added score counter for both players
    - implemented board reset on restart

--- app/src/main/java/com/example/tic_tac_toe/PlayingField.java
+++ app/src/main/java/com/example/tic_tac_toe/PlayingField.java
@@ -1,5 +1,6 @@
 package com.example.tic_tac_toe;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,7 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayingField extends AppCompatActivity {
+    private final List<int[]> combinationList = new ArrayList<>();
+    private TextView playerOneName;
+    private TextView playerTwoName;
     private int activePlayer = 1;
     private int[] boxPositions = {0,0,0,0,0,0,0,0,0};
     private int totalSelectedBoxes = 1;
@@ -27,20 +34,34 @@ public class PlayingField extends AppCompatActivity {
     private ImageView image8;
     private ImageView image9;
 
+    private int currentScoreOne = 0;
+    private int currentScoreTwo = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playing_field);
 
-        TextView playerOneName = findViewById(R.id.playerOneName);
-        TextView playerTwo = findViewById(R.id.playerTwoName);
+        playerOneName = findViewById(R.id.playerOneName);
+        playerTwoName = findViewById(R.id.playerTwoName);
+
+        combinationList.add(new int[]{0,1,2});
+        combinationList.add(new int[]{3,4,5});
+        combinationList.add(new int[]{6,7,8});
+        combinationList.add(new int[]{0,3,6});
+        combinationList.add(new int[]{1,4,7});
+        combinationList.add(new int[]{2,5,8});
+        combinationList.add(new int[]{0,4,8});
+        combinationList.add(new int[]{2,4,6});
+
+        playerOneName = findViewById(R.id.playerOneName);
+        playerTwoName = findViewById(R.id.playerTwoName);
 
         String getPlayerOneName = getIntent().getStringExtra("playerOne");
         String getPlayerTwoName = getIntent().getStringExtra("playerTwo");
 
         playerOneName.setText(getPlayerOneName);
-        playerTwo.setText(getPlayerTwoName);
+        playerTwoName.setText(getPlayerTwoName);
 
         image1 = findViewById(R.id.image1);
         image2 = findViewById(R.id.image2);
@@ -55,86 +76,148 @@ public class PlayingField extends AppCompatActivity {
         image1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performAction((ImageView) v, 0 );
+                if(isBoxSelecteble(0)) {
+                    performAction((ImageView) v, 0);
+                }
             }
         });
 
         image2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performAction((ImageView) v, 1 );
+                if(isBoxSelecteble(1)) {
+                    performAction((ImageView) v, 1);
+                }
             }
         });
 
         image3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performAction((ImageView) v, 2 );
+                if(isBoxSelecteble(2)) {
+                    performAction((ImageView) v, 2);
+                }
             }
         });
 
         image4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performAction((ImageView) v, 3 );
+                if(isBoxSelecteble(3)) {
+                    performAction((ImageView) v, 3);
+                }
             }
         });
 
         image5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performAction((ImageView) v, 4 );
+                if(isBoxSelecteble(4)) {
+                    performAction((ImageView) v, 4);
+                }
             }
         });
 
         image6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performAction((ImageView) v, 5 );
+                if(isBoxSelecteble(5)) {
+                    performAction((ImageView) v, 5);
+                }
             }
         });
 
         image7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performAction((ImageView) v, 6 );
+                if(isBoxSelecteble(6)) {
+                    performAction((ImageView) v, 6);
+                }
             }
         });
 
         image8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performAction((ImageView) v, 7 );
+                if(isBoxSelecteble(7)) {
+                    performAction((ImageView) v, 7);
+                }
             }
         });
 
         image9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performAction((ImageView) v, 8 );
+                if(isBoxSelecteble(8)) {
+                    performAction((ImageView) v, 8);
+                }
             }
         });
 
-
-
     }
-
     private void performAction(ImageView imageView, int selectedBoxPosition){
         boxPositions[selectedBoxPosition] = activePlayer;
         imageView.setBackgroundResource(R.drawable.white_box);
         imageView.setScaleType(ImageView.ScaleType.CENTER);
 
+        playerOneName = findViewById(R.id.playerOneName);
+        playerTwoName = findViewById(R.id.playerTwoName);
+
+        TextView scorePlayerOne = findViewById(R.id.scoreOne);
+        TextView scorePlayerTwo = findViewById(R.id.scoreTwo);
+
         if(activePlayer == 1){
             imageView.setImageResource(R.drawable.ximage);
-            changePlayerTurn(2);
-            totalSelectedBoxes++;
+            if(checkResult()){
+                ResultDialog resultDialog = new ResultDialog(PlayingField.this,
+                        playerOneName.getText().toString()+" is a Winner",
+                        PlayingField.this);
+                resultDialog.setCancelable(false);
+                resultDialog.show();
+                currentScoreOne++;
+                scorePlayerOne.setText(String.valueOf(currentScoreOne));
+            } else if (totalSelectedBoxes == 9) {
+                ResultDialog resultDialog = new ResultDialog(PlayingField.this,
+                        "Math Draw", PlayingField.this);
+                resultDialog.setCancelable(false);
+                resultDialog.show();
+            }else {
+                changePlayerTurn(2);
+                totalSelectedBoxes++;
+            }
         }else{
             imageView.setImageResource(R.drawable.oimage);
-            changePlayerTurn(1);
-            totalSelectedBoxes++;
+            if(checkResult()){
+                ResultDialog resultDialog = new ResultDialog(PlayingField.this,
+                        playerTwoName.getText().toString()+" is a Winner",
+                        PlayingField.this);
+                resultDialog.setCancelable(false);
+                resultDialog.show();
+                currentScoreTwo++;
+                scorePlayerTwo.setText(String.valueOf(currentScoreTwo));
+            } else if (totalSelectedBoxes == 9) {
+                ResultDialog resultDialog = new ResultDialog(PlayingField.this,
+                        "Math Draw", PlayingField.this);
+                resultDialog.setCancelable(false);
+                resultDialog.show();
+            }else {
+                changePlayerTurn(1);
+                totalSelectedBoxes++;
+            }
         }
     }
-
+    private boolean checkResult(){
+        boolean response = false;
+        for (int i = 0; i<combinationList.size(); i++){
+            final int[]combination = combinationList.get(i);
+            if(boxPositions[combination[0]] == activePlayer &&
+                    boxPositions[combination[1]] == activePlayer &&
+                    boxPositions[combination[2]] == activePlayer){
+                response = true;
+            }
+        }
+        return response;
+    }
     private void changePlayerTurn(int currentPlayerTurn){
         activePlayer = currentPlayerTurn;
         LinearLayout playerOneLayoutOuter = findViewById(R.id.playerOneLinearLayout);
@@ -147,5 +230,37 @@ public class PlayingField extends AppCompatActivity {
             playerTwoLayoutOuter.setBackgroundResource(R.drawable.black_border);
             playerOneLayoutOuter.setBackgroundResource(R.drawable.white_box);
         }
+    }
+    private boolean isBoxSelecteble(int boxPosition){
+        boolean response = false;
+        if(boxPositions[boxPosition] == 0){
+            response = true;
+        }
+        return response;
+    }
+    public void restartMath(){
+       boxPositions = new int[]{0,0,0,0,0,0,0,0,0};
+       activePlayer = 1;
+       totalSelectedBoxes = 1;
+
+        image1 = findViewById(R.id.image1);
+        image2 = findViewById(R.id.image2);
+        image3 = findViewById(R.id.image3);
+        image4 = findViewById(R.id.image4);
+        image5 = findViewById(R.id.image5);
+        image6 = findViewById(R.id.image6);
+        image7 = findViewById(R.id.image7);
+        image8 = findViewById(R.id.image8);
+        image9 = findViewById(R.id.image9);
+
+        image1.setImageResource(R.drawable.white_box);
+        image2.setImageResource(R.drawable.white_box);
+        image3.setImageResource(R.drawable.white_box);
+        image4.setImageResource(R.drawable.white_box);
+        image5.setImageResource(R.drawable.white_box);
+        image6.setImageResource(R.drawable.white_box);
+        image7.setImageResource(R.drawable.white_box);
+        image8.setImageResource(R.drawable.white_box);
+        image9.setImageResource(R.drawable.white_box);
     }
 }
